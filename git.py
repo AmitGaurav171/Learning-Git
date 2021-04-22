81929099441a34d75cff484b54e876e62bb92e6d
unknown <amitgaurav171@gmail.com>
2021-04-22 19:16:52 +0530

    third version

--- git.py
+++ git.py
@@ -1,3 +1,5 @@
 print('hey i m learning git and github')
 
 print('I am now making some changes in the code to commit again and push on the remote server')
+
+print('I m again making changes to learn about difftool')
